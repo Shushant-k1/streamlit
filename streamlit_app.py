ed3446c10accede69ae1a0afebfb3fba524d88f9
shushant kumar <rekhaubgb@gmail.com>
2023-03-07 19:26:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.sidebar.info(
     )
 # Set the model engine and your OpenAI API key
 model_engine = "text-davinci-003"
-openai.api_key = "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" #follow step 4 to get a secret_key
+openai.api_key = "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" #follow step 4 to get a secret_key
 def main():
     '''
     This function gets the user input, pass it to ChatGPT function and 
