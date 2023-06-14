abc30d8ecf481f83249a92355757d95775297c57
shushant kumar <rekhaubgb@gmail.com>
2023-06-14 11:18:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,27 @@
 import streamlit as st
-import openai
-st.title("Chatting with ChatGPT")
-st.sidebar.header("Instructions")
-st.sidebar.info(
-    '''This is a web application that allows you to interact with 
-       the OpenAI API's implementation of the ChatGPT model.
-       Enter a **query** in the **text box** and **press enter** to receive 
-       a **response** from the ChatGPT
-       '''
-    )
-# Set the model engine and your OpenAI API key
-model_engine = "text-davinci-003"
-openai.api_key = "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" #follow step 4 to get a secret_key
-def main():
-    '''
-    This function gets the user input, pass it to ChatGPT function and 
-    displays the response
-    '''
-    # Get user input
-    user_query = st.text_input("Enter query here, to exit enter :q", "what is Python?")
-    if user_query != ":q" or user_query != "":
-        # Pass the query to the ChatGPT function
-        response = ChatGPT(user_query)
-        return st.write(f"{user_query} {response}")
-
-def ChatGPT(user_query):
-    ''' 
-    This function uses the OpenAI API to generate a response to the given 
-    user_query using the ChatGPT model
-    '''
-    # Use the OpenAI API to generate a response
-    completion = openai.Completion.create(
-                                  engine = model_engine,
-                                  prompt = user_query,
-                                  max_tokens = 1024,
-                                  n = 1,
-                                  temperature = 0.5,
-                                      )
-    response = completion.choices[0].text
-    return response
-main()
+import easyocr
+import cv2
+from PIL import Image
+def fun(image):
+  reader = easyocr.Reader(['en']) 
+  result = reader.readtext(image)
+  #image = cv2.imread(image)
+  res = reader.readtext(image) 
+  z = ""
+  for (bbox, text, prob) in res: 
+    # unpack the bounding box/
+    (tl, tr, br, bl) = bbox
+    tl = (int(tl[0]), int(tl[1]))
+    tr = (int(tr[0]), int(tr[1]))
+    br = (int(br[0]), int(br[1]))
+    bl = (int(bl[0]), int(bl[1]))
+    cv2.rectangle(image, tl, br, (255, 0,0), 2)
+    z = z + text
+  return z
+img = st.file_uploader("Choose a Image", accept_multiple_files=False)
+if img is not None :
+    image = Image.open(img)
+    st.image(image, caption='Sunrise by the mountains')
+    #img = cv2.imread(image)
+    final = fun(image)
+    st.write(final)
