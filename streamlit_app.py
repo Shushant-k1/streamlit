a1fe602c045319a990fcbf82dab4f38ca5eacf70
shushant kumar <rekhaubgb@gmail.com>
2023-06-14 11:23:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,12 @@ def fun(image):
   z = ""
   for (bbox, text, prob) in res: 
     # unpack the bounding box/
-    (tl, tr, br, bl) = bbox
-    tl = (int(tl[0]), int(tl[1]))
-    tr = (int(tr[0]), int(tr[1]))
-    br = (int(br[0]), int(br[1]))
-    bl = (int(bl[0]), int(bl[1]))
-    cv2.rectangle(image, tl, br, (255, 0,0), 2)
+#     (tl, tr, br, bl) = bbox
+#     tl = (int(tl[0]), int(tl[1]))
+#     tr = (int(tr[0]), int(tr[1]))
+#     br = (int(br[0]), int(br[1]))
+#     bl = (int(bl[0]), int(bl[1]))
+#     cv2.rectangle(image, tl, br, (255, 0,0), 2)
     z = z + text
   return z
 img = st.file_uploader("Choose a Image", accept_multiple_files=False)
